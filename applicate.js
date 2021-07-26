27e01328f4abb121f81104a1fc3c0dacc3fc8d2c
NGUYỄN THANH LƯU <75246419+NguyenThanhLuu@users.noreply.github.com>
2021-07-26 14:36:28 +0700

    update_changefile.js
    this is my new change and that the time i change and people can see and for me idea

--- applicate.js
+++ applicate.js
@@ -2,3 +2,6 @@ alert("this is my change")
 // this content i will save again 
 var name = 'thanh';
 document.getElementById('change').innerHTML = 'sin_change';
+// this is my change and you can refers and have me idea and this change 
+alert('change that name ');
+alert(45);
